ba55be4ea3101de95a01f6449814a2c5583012a8
idbis <idbis@idbis-SATELLITE-PRO-A50-C>
2020-05-18 13:24:00 +0200

    Merge origin/master
    Conflicts:
    	src/Evaluacion.java

--- src/CorregirExamen.java
+++ src/CorregirExamen.java
@@ -4,7 +4,7 @@ import examen.mayo.*;
 public class CorregirExamen {
 
     /*
-        Debéis crear un objeto de la clase que habéis creado que implemente Corregible, y añadirlo a la lista.
+        Debéis crear un objeto de la clase Nueva que habéis creado, y hacer que sustituya a vuestra clase Borrame.
     
      */
     public static void main(String[] args) {
